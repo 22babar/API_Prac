2d027e66c85e6fe37ec9aabee4cf36bd8b8a4da8
MR BABAR ALI <135489014+22babar@users.noreply.github.com>
2024-10-31 14:49:14 +0500

    Add files via upload

---
+++ formData API/script.js
@@ -0,0 +1,17 @@
+let form = document.getElementById("form");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        // console.log(formData);
+        for (const keyValue of formData.entries()) {
+            console.log(keyValue[0], ":" , keyValue[1]);
+        }
+})
+
+
+
+
+
+
